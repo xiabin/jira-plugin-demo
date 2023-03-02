bb1ac810bca80b1b2d76b030ec23da4f9b86b8e7
xiabin <xiabin@weixiuhui.cn>
2023-03-03 07:18:19 +0800

    添加时间转换demo

--- src/main/java/com/example/demo/impl/Test.java
+++ src/main/java/com/example/demo/impl/Test.java
@@ -8,11 +8,11 @@ import java.util.Locale;
 public class Test {
     public static void main(String[] args) {
         try {
-            String dateString = "Sun Mar 26 2023 00:00:00 GMT+0800 (China Standard Time)";
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd yyyy HH:mm:ss 'GMT'Z '('z')'", Locale.ENGLISH);
-            LocalDate localDate = LocalDate.parse(dateString, formatter);
-
-            System.out.printf(localDate.toString());
+            long timestamp = 1679760000000L; // 假设时间戳为2022-03-25 00:00:00
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MMM/yy");
+            Instant instant = Instant.ofEpochMilli(timestamp);
+            LocalDate localDate = instant.atZone(ZoneId.systemDefault()).toLocalDate();
+            System.out.println(localDate.format(formatter));
         } catch (DateTimeParseException ex) {
             ex.printStackTrace();
         }
