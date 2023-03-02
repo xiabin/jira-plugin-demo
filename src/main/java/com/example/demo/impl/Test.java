294c3f7ca405d97792f8dc0888bbf16eb20b386f
xiabin <276147613@qq.com>
2023-03-02 19:24:01 +0800

    完成修改开始结束时间接口

--- src/main/java/com/example/demo/impl/Test.java
+++ src/main/java/com/example/demo/impl/Test.java
@@ -1,20 +1,20 @@
 package com.example.demo.impl;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Locale;
 
 public class Test {
     public static void main(String[] args) {
-        String goodFriday = "Feb 13, 2023 12:00:00 AM";
         try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d, yyyy h:mm:ss a");
-            LocalDate holiday = LocalDate.parse(goodFriday, formatter);
-            System.out.printf("Successfully parsed String %s, date is %s%n", goodFriday, holiday);
+            String dateString = "Sun Mar 26 2023 00:00:00 GMT+0800 (China Standard Time)";
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd yyyy HH:mm:ss 'GMT'Z '('z')'", Locale.ENGLISH);
+            LocalDate localDate = LocalDate.parse(dateString, formatter);
+
+            System.out.printf(localDate.toString());
         } catch (DateTimeParseException ex) {
-            System.out.printf("%s is not parsable!%n", goodFriday); ex.printStackTrace();
+            ex.printStackTrace();
         }
     }
 }
